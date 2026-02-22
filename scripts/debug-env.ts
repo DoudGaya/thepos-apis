ba92c7861866d82022ec94dab2e9a1d74cb45b24
Abdulrahman Dauda Gaya <57319135+DoudGaya@users.noreply.github.com>
2026-02-22 11:09:20 +0100

    fix

--- scripts/debug-env.ts
+++
@@ -1,5 +0,0 @@
-
-console.log('VTU_NG_USERNAME:', process.env.VTU_NG_USERNAME)
-console.log('AMIGO_API_TOKEN:', process.env.AMIGO_API_TOKEN)
-console.log('CLUBKONNECT_USER_ID:', process.env.CLUBKONNECT_USER_ID)
-console.log('SUBANDGAIN_USERNAME:', process.env.SUBANDGAIN_USERNAME)
